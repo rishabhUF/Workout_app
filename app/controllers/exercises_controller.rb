4aab15cc6e2c1c67fff9cc546c0448a1b8854b08
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-12-04 23:16:33 -0500

    Project completed

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,12 @@
+class ExercisesController < ApplicationController
+
+	def create
+		@workout = Workout.find(params[:workout_id])
+		@exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
+
+		redirect_to workout_path(@workout)
+	end
+
+	def update
+	end
+end
